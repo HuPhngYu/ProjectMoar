8cfc479acffe5041a60670ffe6b2a1129c86f1e3
duhue <duhue@10.48.239.152>
2017-12-28 21:19:41 +0700

    Add save/load/delete functions

--- MoarAdams/public_html/js/game.js
+++ MoarAdams/public_html/js/game.js
@@ -12,10 +12,31 @@ var puppyCost = 100;
 var boyfriendCost = 500;
 var concertCost = 1000;
 
+// prettify numbers
+function prettify(input) {
+    var output = Math.round(input * 1000000) / 1000000;
+    return output;
+}
+// load all number
+function loadAll() {
+    document.getElementById('adams').innerHTML = prettify(adams);
+    document.getElementById('cursors').innerHTML = prettify(cursors);
+    document.getElementById('cursorCost').innerHTML = prettify(cursorCost);
+    document.getElementById('puppies').innerHTML = prettify(puppies);
+    document.getElementById('puppyCost').innerHTML = prettify(puppyCost);
+    document.getElementById('boyfriends').innerHTML = prettify(boyfriends);
+    document.getElementById('boyfriendCost').innerHTML = prettify(boyfriendCost);
+    document.getElementById('concerts').innerHTML = prettify(concerts);
+    document.getElementById('concertCost').innerHTML = prettify(concertCost);
+}
+;
+
+// buy items functions
 function getAdam(number) {
     adams = adams + number;
-    document.getElementById('adams').innerHTML = adams;
-};
+    document.getElementById('adams').innerHTML = prettify(adams);
+}
+;
 
 function buyCursor() {
     if (adams >= cursorCost) {
@@ -26,7 +47,8 @@ function buyCursor() {
         cursorCost = Math.round(cursorCost * 115 / 100);
     }
     document.getElementById('cursorCost').innerHTML = cursorCost;
-};
+}
+;
 
 function buyPuppy() {
     if (adams >= puppyCost) {
@@ -37,7 +59,8 @@ function buyPuppy() {
         puppyCost = Math.round(puppyCost * 115 / 100);
     }
     document.getElementById('puppyCost').innerHTML = puppyCost;
-};
+}
+;
 
 function buyBoyfriend() {
     if (adams >= boyfriendCost) {
@@ -48,7 +71,8 @@ function buyBoyfriend() {
         boyfriendCost = Math.round(boyfriendCost * 115 / 100);
     }
     document.getElementById('boyfriendCost').innerHTML = boyfriendCost;
-};
+}
+;
 
 function buyConcert() {
     if (adams >= concertCost) {
@@ -59,9 +83,75 @@ function buyConcert() {
         concertCost = Math.round(concertCost * 115 / 100);
     }
     document.getElementById('concertCost').innerHTML = concertCost;
-};
+}
+;
 
-window.setInterval(function(){
+// save game
+function save() {
+    //if (!supportsLocalStorage()) { return false; }
+    var newSave = {
+        adams: adams,
+        cursors: cursors,
+        puppies: puppies,
+        boyfriends: boyfriends,
+        concerts: concerts,
+        cursorCost: cursorCost,
+        puppyCost: puppyCost,
+        boyfriendCost: boyfriendCost,
+        concertCost: concertCost
+    };
+    localStorage.setItem("newSave", JSON.stringify(newSave));
+    document.getElementById('fadeout-alert').style.display = "block";
+    document.getElementById('message').innerHTML = "Game is saved";
+}
+;
+
+// load game
+function load() {
+    var oldSave = JSON.parse(localStorage.getItem("newSave"));
+    if (typeof oldSave.adams !== "undefined") {
+        adams = oldSave.adams;
+    }
+    if (typeof oldSave.cursors !== "undefined") {
+        cursors = oldSave.cursors;
+        cursorCost = oldSave.cursorCost;
+    }
+    if (typeof oldSave.puppies !== "undefined") {
+        puppies = oldSave.puppies;
+        puppyCost = oldSave.puppyCost;
+    }
+    if (typeof oldSave.boyfriends !== "undefined") {
+        boyfriends = oldSave.boyfriends;
+        boyfriendCost = oldSave.boyfriendCost;
+    }
+    if (typeof oldSave.concerts !== "undefined") {
+        concerts = oldSave.concerts;
+        concertCost = oldSave.concertCost;
+    }
+    loadAll();
+    document.getElementById('fadeout-alert').style.display = "block";
+    document.getElementById('message').innerHTML = "Game is loaded";
+}
+
+// DELETE SAVE
+function deleteSave() {
+    localStorage.removeItem("newSave");
+    adams = 0;
+    cursors = 0;
+    puppies = 0;
+    boyfriends = 0;
+    concerts = 0;
+    cursorCost = 10;
+    puppyCost = 100;
+    boyfriendCost = 500;
+    concertCost = 1000;
+    loadAll();
+    document.getElementById('fadeout-alert').style.display = "block";
+    document.getElementById('message').innerHTML = "Save is deleted";
+}
+
+// adam counters
+window.setInterval(function() {
     persec = cursors + (puppies * 10) + (boyfriends * 50) + (concerts * 100);
     getAdam(persec);
     document.getElementById("persec").innerHTML = persec;
